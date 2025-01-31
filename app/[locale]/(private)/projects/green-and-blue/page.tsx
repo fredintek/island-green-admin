c8e37c574d6c0ef38c0c092df98949fe7072a21f
Arinze Dibor Alfred <arinze.diboralfred@neareasttech.local>
2025-01-31 15:41:46 +0200

    working on projects

--- app/[locale]/(private)/projects/green-and-blue/page.tsx
+++ app/[locale]/(private)/projects/green-and-blue/page.tsx
@@ -1,12 +1,24 @@
+import DisplayContent from "@/components/projects/DisplayContent";
+import ProductLink from "@/components/projects/ProductLink";
+import ProjectHouses from "@/components/projects/ProjectHouses";
 import React from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
   return (
-    <div>
-      <p>PROJECTS GREEN AND BLUE</p>
-    </div>
+    <section className="flex flex-col gap-10">
+      {/* product link section */}
+      <ProductLink />
+
+      {/* display content */}
+      <DisplayContent />
+
+      {/* project houses */}
+      <ProjectHouses />
+
+      {/* stage 2 */}
+    </section>
   );
 };
 
